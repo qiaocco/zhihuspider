eae78952d823a9be28d681f1357fb70dea19fb77
qiaocc <jasonqiao36@gmail.com>
2019-11-25 16:44:43 +0800

    增加 用户粉丝抓取任务

--- tasks/user.py
+++ tasks/user.py
@@ -1,12 +1,27 @@
 from db.models import SeedUser
 from logger import crawler
-from page_get import get_profile
+from page_get import get_fans_or_followers_names, get_profile
 from tasks.workers import app
 
 
 @app.task
+def crawl_follower_fans(name):
+    """
+    抓取用户粉丝信息
+    :param name: 用户名
+    :return:
+    """
+    seed = SeedUser.get_seed_by_name(name)
+    if seed.other_crawled == 0:
+        get_fans_or_followers_names(name, "followees")  # 关注
+        get_fans_or_followers_names(name, "followers")  # 粉丝
+
+    SeedUser.set_other_crawled(name, 1)
+
+
+@app.task
 def crawl_user_info(name):
-    """抓取用户及粉丝和关注者的信息
+    """抓取用户首页的信息
     :param name: 用户名
     :return: None
     """
@@ -14,16 +29,18 @@ def crawl_user_info(name):
         return None
 
     crawler.info(f"received task crawl_user_info {name}")
-    user, is_crawled = get_profile(name)
-    if not is_crawled:
+    user, other_crawled = get_profile(name)
+    if not other_crawled:
         crawler.info(f"send task crawl_follower_fans {user.name}")
         app.send_task("tasks.user.crawl_follower_fans", args=(user.name,))
 
 
 @app.task
 def execute_user_task():
-    seeds = SeedUser.get_seed_names()
-    if seeds:
+    seeds, is_exists = SeedUser.get_seed_names()
+    if is_exists:
         for seed in seeds:
             crawler.info(f"send task crawl_user_info {seed.name}")
             app.send_task("tasks.user.crawl_user_info", args=(seed.name,))
+    else:
+        crawler.info("find no user, abort")
